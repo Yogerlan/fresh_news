83a09e9606920df0cf8ec8a8492cc90df2dca61e
Yogerlan Ledesma Almanza <yogerlan@gmail.com>
2024-06-21 17:36:40 -0600

    Input WorkItems added.

--- tasks.py
+++ tasks.py
@@ -1,6 +1,16 @@
+import logging
+
 from robocorp.tasks import task
+from RPA.Robocorp.WorkItems import WorkItems
+
 
 @task
 def news_collector():
-    message = "Hello"
-    message = message + " World!"
+    try:
+        wi = WorkItems()
+        wi.get_input_work_item()
+
+        for key, value in wi.get_work_item_variables().items():
+            logging.info(f"{key} = {value}")
+    except Exception as ex:
+        logging.info("No workitems.")
